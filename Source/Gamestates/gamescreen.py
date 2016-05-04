45b39e8fca6341e69c3772c5914f8c78fb9545f8
Richard Selneck <raselneck@gmail.com>
2016-05-04 09:01:54 -0400

    Fix bug with game screen UI being created more than once

--- Source/Gamestates/gamescreen.py
+++ Source/Gamestates/gamescreen.py
@@ -9,31 +9,33 @@ class GameScreen:
 
         self.uiContainer = UIContainer(self.window)
         self.uiContainer.horizontalStride = 5
+        self.initializedUI = False
 
         self.screenInfo = pygame.display.Info()
         pass
 
     def start(self):
-        # Button sizes
-        padding = 10
-        side_margin = 50
-        top_margin = 150
-        width = (self.screenInfo.current_w - (padding * 5) - (side_margin * 2)) / 5
-        height = (self.screenInfo.current_h - (padding * 5) - (top_margin)) / 5
+        if not self.initializedUI:
+            # Button sizes
+            padding = 10
+            side_margin = 50
+            top_margin = 150
+            width = (self.screenInfo.current_w - (padding * 5) - (side_margin * 2)) / 5
+            height = (self.screenInfo.current_h - (padding * 5) - (top_margin)) / 5
 
-        for i in range(0, 5):
-            for j in range(0, 5):
-                button = self.uiContainer.add_button()
-                # button.rect = pygame.Rect((padding * (i + 1)) + (width * i), (padding * (j + 1)) + (height * j), width, height)
-                button.rect = pygame.Rect((padding * i) + (width * i) + side_margin, (padding * j) + (height * j) + top_margin, width, height)
-        pass
+            for i in range(0, 5):
+                for j in range(0, 5):
+                    button = self.uiContainer.add_button()
+                    # button.rect = pygame.Rect((padding * (i + 1)) + (width * i), (padding * (j + 1)) + (height * j), width, height)
+                    button.rect = pygame.Rect((padding * i) + (width * i) + side_margin, (padding * j) + (height * j) + top_margin, width, height)
+
+            self.initializedUI = True
         
     def update(self):
         if (xo_input.btn_cross):
             self.stateManager.switchGameState("MainScreen")
 
         self.uiContainer.update()
-        pass
     
     def draw(self):
         pygame.draw.rect(self.window, (255, 255, 255), (50, 50, 50, 50), 0)
